22e2e9b4fe9675d4c51c0492e589dd4420cb6b68
roshanigovindvishwakarma-cell <roshanigovindvishwakarma@gmail.com>
2026-03-22 11:00:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("regForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    document.getElementById("message").innerText = "Registration Successful!";
+});
